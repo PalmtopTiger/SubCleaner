913e55a21109b65e92981681aef9beafb4bf9d44
Andrey Efremov <duxus@yandex.ru>
2019-01-31 13:26:12 +0700

    Update license

--- src/script.cpp
+++ src/script.cpp
@@ -1,21 +1,20 @@
-/*******************************************************************************
+/*
  * This file is part of Re_Sync Next.
- *
  * Copyright (C) 2011-2019  Andrey Efremov <duxus@yandex.ru>
  *
- * Re_Sync is free software: you can redistribute it and/or modify
+ * Re_Sync Next is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Re_Sync is distributed in the hope that it will be useful,
+ * Re_Sync Next is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Re_Sync.  If not, see <http://www.gnu.org/licenses/>.
- ******************************************************************************/
+ * along with Re_Sync Next.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 #include "script.h"
 #include <QHash>
